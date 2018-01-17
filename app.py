5bc2bf349e2761a6c0819ba4f68da9a179a01fc0
James Xu <jrx2000@columbia.edu>
2018-01-17 17:47:43 -0500

    completed challenge

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/jrx2000', methods=['GET'])
+def get_website():
+    return render_template('jrx2000.html')
+
 
 if __name__ == '__main__':
     app.run()
